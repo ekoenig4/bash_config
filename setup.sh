3dbcabc4d9ee9aa7c997c3b4549cd2b190999b7b
ekoenig4 <ekoenig4@wisc.edu>
2024-02-05 18:15:44 +0100

    util scripts

--- setup.sh
+++ setup.sh
@@ -1,16 +1,6 @@
-#!/bin/sh
-
-REPO=$(dirname $0)
-
 CFGS=(
     bash_profile
     bashrc
     userrc
     zshrc
 )
-
-for CFG in ${CFGS[@]}; do
-    if [ -f $REPO/$CFG ]; then
-        cp -v $REPO/$CFG ~/.$CFG
-    fi
-done
